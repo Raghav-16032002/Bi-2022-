d00b5a7f5cd4a3c83cc3360d4ffa9a286ad8afe0
Raghav Somani <raghavsomani27205@gmail.com>
2022-09-11 21:22:10 +0530

    Advance FrontEnd Assignment

--- Advance FrontEnd/Assignment-1/Problem3.js
+++ Advance FrontEnd/Assignment-1/Problem3.js
@@ -15,10 +15,22 @@ function generateCard() {
     const locationValue = document.getElementById("location").value;
     // Assign the value of location name to generated card
     document.getElementById("cardLocation").innerHTML = locationValue;
-    // Get value of location name from form input 
-    const ImageValue = document.getElementById("Image").value;
-    // Assign the value of location name to generated card
-    document.getElementById("cardImage").style.display = ImageValue;
+    // Get value of Image name from form input 
+    // const ImageValue = document.getElementById("Image").value;
+    // // Assign the value of Image to generated card
+    // document.getElementById("cardImage").style.display = ImageValue;
     // Display final generated card to user     
     document.getElementById("collegeCard").style.display = "block";
 }
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+  
+      reader.onload = function (e) {
+        console.log(e.target.result);
+        document.getElementById('blah').setAttribute("src",e.target.result);
+      };
+  
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
